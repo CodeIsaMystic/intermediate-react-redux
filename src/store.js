eecf5b35897828e735dd71320fd9045bc4ac75ee
Julien Orjollet <oj.professionnel@gmail.com>
2020-11-09 14:31:19 +0100

    created store,reducers, actions then connecting redux to the app

--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { createStore } from "redux";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  /** if we're in the browser and redux dev tools exist
+   *   ==> use it 
+   *   ==> if not use this bug's function
+   * ( useful also for Saga or Thunks )
+  **/
+  typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+);
 
-/** if we're in the browser and redux dev tools exist
- *   ==> use it 
- *   ==> if not use this bug's function
- * ( useful also for Saga or Thunks )
-**/
-typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
-  ? window.__REDUX_DEVTOOLS_EXTENSION__()
-  : f => f
 
 
-export default import("redux").Store;
+export default store;
